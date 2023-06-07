a94171888d07e87a8ae7c6cb61de0bc09fb27cb9
BKJang <jp302119@gmail.com>
2023-06-07 20:42:06 +0900

    feat(core): Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@babel/eslint-parser',
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    requireConfigFile: false,
+  },
+  plugins: [],
+  rules: {},
+};
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      리액트로 포트폴리오 만들기
+    </div>
+  );
+}
+
+export default App;
