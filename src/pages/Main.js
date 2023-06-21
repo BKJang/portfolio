98afc19f66606f1e16e42e40a5c7d8cb34d560d4
BKJang <jp302119@gmail.com>
2023-06-21 23:34:24 +0900

    feat(core): 이미지슬라이드 애니메이션 구현

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -12,6 +12,98 @@ function Contact($container) {
     this.$container.innerHTML = `
     <div class="main_box">
       <div id="lottie_sample"></div>
+      <div class="slide_parent">
+        <div class="slide_element primary">
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/damilola-jerugba.svg"
+            alt="damilola jerugba"
+          />
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/tosin-jerugba.svg"
+            alt="tosin jerugba"
+          />
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/fiyin-jerugba.svg"
+            alt="fiyin jerugba"
+          />
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/feyikemi-jerugba.svg"
+            alt="feyikemi jerugba"
+          />
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/femi-jerugba.svg"
+            alt="femi jerugba"
+          />
+        </div>
+        <div class="slide_element secondary">
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/damilola-jerugba.svg"
+            alt="damilola jerugba"
+          />
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/tosin-jerugba.svg"
+            alt="tosin jerugba"
+          />
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/fiyin-jerugba.svg"
+            alt="fiyin jerugba"
+          />
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/feyikemi-jerugba.svg"
+            alt="feyikemi jerugba"
+          />
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/femi-jerugba.svg"
+            alt="femi jerugba"
+          />
+        </div>
+      </div>
+      <div class="slide_parent">
+        <div class="slide_element primary_left">
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/damilola-jerugba.svg"
+            alt="damilola jerugba"
+          />
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/tosin-jerugba.svg"
+            alt="tosin jerugba"
+          />
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/fiyin-jerugba.svg"
+            alt="fiyin jerugba"
+          />
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/feyikemi-jerugba.svg"
+            alt="feyikemi jerugba"
+          />
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/femi-jerugba.svg"
+            alt="femi jerugba"
+          />
+        </div>
+        <div class="slide_element secondary_left">
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/damilola-jerugba.svg"
+            alt="damilola jerugba"
+          />
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/tosin-jerugba.svg"
+            alt="tosin jerugba"
+          />
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/fiyin-jerugba.svg"
+            alt="fiyin jerugba"
+          />
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/feyikemi-jerugba.svg"
+            alt="feyikemi jerugba"
+          />
+          <img
+            src="https://avatars.dicebear.com/api/adventurer/femi-jerugba.svg"
+            alt="femi jerugba"
+          />
+        </div>
+      </div>
     </div>
     `;
   };
